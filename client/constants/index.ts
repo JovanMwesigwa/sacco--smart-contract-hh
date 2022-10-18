c64f90915c4ff02c618c2934b3ce0efdb75ed7ff
JovanMwesigwa <jovanmwesigwa79@gmail.com>
2022-10-18 18:12:32 +0300

    Fixed artifacts path

--- client/constants/index.ts
+++ client/constants/index.ts
@@ -1,4 +1,4 @@
-import abi from '../artifacts/contracts/Sacco.sol/Sacco.json'
+import abi from '../backend/contracts/Sacco.sol/Sacco.json'
 
 export const CONTRACT_ADDRESS = "0x95489c8fae0f19D4cE12fB130577F69Cabcdf8cE"
 export const ABI = abi.abi
