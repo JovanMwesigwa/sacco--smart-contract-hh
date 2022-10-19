38e913121f82ebadfbb530ef57693b00783b16d3
JovanMwesigwa <jovanmwesigwa79@gmail.com>
2022-10-19 03:28:12 +0300

    Fixed css import at _app

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,9 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { MoralisProvider } from 'react-moralis'
 import { ToastProvider, useToasts } from 'react-toast-notifications'
-
 import { NotificationProvider } from 'web3uikit'
+
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
